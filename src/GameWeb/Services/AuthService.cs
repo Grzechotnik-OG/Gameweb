326497491f3b9e050532390b56f0adc8e60e5dca
pduda98 <pduda.98@gmail.com>
2020-12-28 17:24:28 +0100

    add user controller methods implementation

--- src/GameWeb/Services/AuthService.cs
+++ src/GameWeb/Services/AuthService.cs
@@ -1,7 +1,16 @@
+using GameWeb.Models;
+
 namespace GameWeb.Services
 {
     public class AuthService : IAuthService
     {
-        
+        public TokenDTO GenerateToken(User user)
+        {
+            throw new System.NotImplementedException("Not implemented");
+        }
+        public void RemoveRefreshTokenById(long id)
+        {
+            throw new System.NotImplementedException("Not implemented");
+        }
     }
 }
