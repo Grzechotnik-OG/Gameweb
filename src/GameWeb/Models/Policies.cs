0818025f3fc83045a54d54624a8be944a7c19c87
pduda98 <pduda.98@gmail.com>
2021-01-09 18:08:03 +0100

    add ModPolicy

--- src/GameWeb/Models/Policies.cs
+++ src/GameWeb/Models/Policies.cs
@@ -6,6 +6,7 @@ namespace GameWeb.Models
     {
         public const string Admin = "Admin";
         public const string User = "User";
+        public const string Mod = "Mod";
         public static AuthorizationPolicy AdminPolicy()
         {
             return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(Admin).Build();
@@ -13,6 +14,10 @@ namespace GameWeb.Models
         public static AuthorizationPolicy UserPolicy()
         {
             return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(User).Build();
+        }
+        public static AuthorizationPolicy ModPolicy()
+        {
+            return new AuthorizationPolicyBuilder().RequireAuthenticatedUser().RequireRole(Mod).Build();
         }
     }
 }
